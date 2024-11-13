8efa93cb2ec8947fd32cf2ef0db961dd250130aa
saicharan2005 <kundurusaicharan@gmail.com>
2024-11-13 20:53:55 +0530

    use setState and eventHandlers

--- smarter-tasks/src/TaskForm.tsx
+++ smarter-tasks/src/TaskForm.tsx
@@ -3,23 +3,33 @@ interface TaskFormProps {
   
 }
 interface TaskFormState {
+  title: string;
 }
 
 class TaskForm extends React.Component<TaskFormProps, TaskFormState> {
   constructor(props: TaskFormProps) {
     super(props);
+    this.state = {
+      title: ""
+    }
   }
   addTask: React.FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
-    console.log(`Submitted the form with ${this.inputRef.current?.value}`);
+    console.log(`Submitted the form with ${this.state.title}`);
   };
-  inputRef = React.createRef<HTMLInputElement>();
+
+  titleChanged: React.ChangeEventHandler<HTMLInputElement> = (event) => {
+    console.log(`${event.target.value}`);
+    this.setState({ title: event.target.value });
+  };
+
+
   render(){
     return (
       <form onSubmit={this.addTask}>
-  <input type="text" ref={this.inputRef} />
-  <button type="submit">Add item</button>
-</form>
+        <input type="text" value={this.state.title} onChange={this.titleChanged}/>
+        <button type="submit">Add item</button>
+      </form>
     )
   }
 }
