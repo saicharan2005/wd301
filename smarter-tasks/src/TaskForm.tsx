c6da0afb05b5235d1a87facf0a2a5af1ce45321c
saicharan2005 <kundurusaicharan@gmail.com>
2024-11-13 20:43:51 +0530

    Handling form submissions

--- smarter-tasks/src/TaskForm.tsx
+++ smarter-tasks/src/TaskForm.tsx
@@ -8,12 +8,16 @@ class TaskForm extends React.Component<TaskFormProps, TaskFormState> {
   constructor(props: TaskFormProps) {
     super(props);
   }
+  addTask: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    console.log("Submitted the form!");
+  };
   render(){
     return (
-      <form>
-        <input type="text" />
-        <button type="submit">Add item</button>
-      </form>
+      <form onSubmit={this.addTask}>
+  <input type="text" />
+  <button type="submit">Add item</button>
+</form>
     )
   }
 }
