925ba8759537ab4184df888355b0968d72b220b9
saicharan2005 <kundurusaicharan@gmail.com>
2025-01-26 18:00:20 +0530

    used react-hook-form

--- smarter-tasks/src/pages/members/NewMember.tsx
+++ smarter-tasks/src/pages/members/NewMember.tsx
@@ -5,13 +5,24 @@
 import { Dialog, Transition } from '@headlessui/react'
 import { Fragment, useState } from "react";
 import { API_ENDPOINT } from "../../config/constants";
+import { useForm, SubmitHandler } from "react-hook-form";
 
+type Inputs = {
+    name: string;
+    email: string;
+    password: string;
+};
 
 
 const NewMember = () => {
 // Then we will use useState hook to handle local state for dialog component
 
     const [isOpen, setIsOpen] = useState(false)
+    const {
+      register,
+      handleSubmit,
+      formState: { errors },
+    } = useForm<Inputs>();
     
     // Then we add the openModal function. 
 // If you don't know, Modal and Dialog are almost same thing.
@@ -25,42 +36,45 @@ const NewMember = () => {
     setIsOpen(false)
   }
     
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-     const [password, setPassword] = useState("");
+    // const [name, setName] = useState("");
+    // const [email, setEmail] = useState("");
+    //  const [password, setPassword] = useState("");
 
 
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      const token = localStorage.getItem("authToken") ?? "";
-      try {
-        const response = await fetch(`${API_ENDPOINT}/users`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-          body: JSON.stringify({ name ,email,password}),
-        });
-        // If response is not OK, in that case I'll throw an error.
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+      // event.preventDefault();
+      
 
-        if (!response.ok) {
-          throw new Error("Failed to create project");
-        }
-        // Next, I'll extract the response body as JSON data
+       const { name,email,password } = data;
+    const token = localStorage.getItem("authToken") ?? "";
+    try {
+      const response = await fetch(`${API_ENDPOINT}/users`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ name, email, password }),
+      });
+      // If response is not OK, in that case I'll throw an error.
 
-        const data = await response.json();
-
-        // Let's print the data in console
-
-          console.log(data);
-          setIsOpen(false);
-      } catch (error) {
-        // And in catch block, I'll print the error in console.
-
-        console.error("Operation failed:", error);
+      if (!response.ok) {
+        throw new Error("Failed to create project");
       }
-    };
+      // Next, I'll extract the response body as JSON data
+
+      const data = await response.json();
+
+      // Let's print the data in console
+
+      console.log(data);
+      setIsOpen(false);
+    } catch (error) {
+      // And in catch block, I'll print the error in console.
+
+      console.error("Operation failed:", error);
+    }
+  };
 
 
     
@@ -105,40 +119,43 @@ const NewMember = () => {
                     Create new member
                   </Dialog.Title>
                   <div className="mt-2">
-                    <form onSubmit={handleSubmit}>
+                    <form onSubmit={handleSubmit(onSubmit)}>
                       <input
                         type="text"
                         required
                         placeholder="Enter member name..."
                         autoFocus
-                        name="name"
                         id="name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        className="w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue"
+                        {...register("name", { required: true })}
+                        className={`w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
+                          errors.name ? "border-red-500" : ""
+                        }`}
                       />
+                      {errors.name && <span>This field is required</span>}
                       <input
                         type="email"
                         required
                         placeholder="Enter member email..."
                         autoFocus
-                        name="email"
                         id="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        className="w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue"
+                        {...register("email", { required: true })}
+                        className={`w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
+                          errors.email ? "border-red-500" : ""
+                        }`}
                       />
+                      {errors.email && <span>This field is required</span>}
                       <input
                         type="password"
                         required
                         placeholder="Enter password..."
                         autoFocus
-                        name="password"
                         id="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        className="w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue"
+                        {...register("password", { required: true })}
+                        className={`w-full border rounded-md py-2 px-3 my-4 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
+                          errors.password ? "border-red-500" : ""
+                        }`}
                       />
+                      {errors.password && <span>This field is required</span>}
                       <button
                         type="submit"
                         className="inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 mr-2 text-sm font-medium text-white hover:bg-blue-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
@@ -153,7 +170,6 @@ const NewMember = () => {
                         Cancel
                       </button>
                     </form>
-                    
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
